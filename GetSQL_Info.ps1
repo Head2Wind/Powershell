6a8878891f1b2845aa0f562f31b9cc3e9b53faf6
Head2Wind <31043620+Head2Wind@users.noreply.github.com>
2020-02-23 11:50:58 -0800

    Add files via upload

---
+++ GetSQL_Info.ps1
@@ -0,0 +1,14 @@
+﻿cls
+# Test to see if the SQLPS module is loaded, and if not, load it
+if (-not(Get-Module -name 'SQLPS')) {
+  if (Get-Module -ListAvailable | Where-Object {$_.Name -eq 'SQLPS' }) {
+      Push-Location # The SQLPS module load changes location to the Provider, so save the current location
+Import-Module -Name 'SQLPS' -DisableNameChecking
+Pop-Location # Now go back to the original location
+    }
+  }
+  $server = 'ServerNameHere'
+  $svr = new-object ('Microsoft.SqlServer.Management.Smo.Server') $server
+$svr | Get-Member
+$svr | select Name, Edition, BuildNumber, Product, ProductLevel, Version, Processors, PhysicalMemory, DefaultFile, DefaultLog, MasterDBPath, MasterDBLogPath, BackupDirectory, ServiceAccount, RootDirectory | Out-File C:\Scripts\SQL_inventory\$server-Info.txt -width 120
+$svr.Databases | select name | Format-Table | Out-File C:\Scripts\SQL_inventory\$server-DBs.txt -width 120
