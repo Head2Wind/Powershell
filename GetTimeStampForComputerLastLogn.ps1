c2fba5d7c9fe0a370bed4fd154985f25d3728245
Head2Wind <31043620+Head2Wind@users.noreply.github.com>
2020-09-10 08:33:13 -0700

    Add files via upload

---
+++ GetTimeStampForComputerLastLogn.ps1
@@ -0,0 +1,12 @@
+﻿# Gets time stamps for all computers in the domain that have NOT logged in after specified number of days stated in 'DaysInactive' variable 
+# Mod by Ken 2013-12-9 
+import-module activedirectory  
+$domain = "enter.domain.name.root"  
+$DaysInactive = 95  
+$time = (Get-Date).Adddays(-($DaysInactive)) 
+  
+# Get all AD computers with lastLogonTimestamp less than our time 
+Get-ADComputer -Filter {LastLogonTimeStamp -lt $time} -Properties LastLogonTimeStamp | 
+  
+# Output hostname and lastLogonTimestamp into CSV 
+select-object Name,@{Name="Stamp"; Expression={[DateTime]::FromFileTime($_.lastLogonTimestamp)}} | export-csv -path c:\scripts\oldcomputers.csv -notypeinformation
